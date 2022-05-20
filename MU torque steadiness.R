3d925314bf6d3a236c41cddba9e314601b57abd7
Gabriel L. Fernandes <86408652+GabeFernandess@users.noreply.github.com>
2022-05-20 17:05:52 +1000

    Update MU torque steadiness.R

--- MU torque steadiness.R
+++ MU torque steadiness.R
@@ -1,18 +1,20 @@
 
+
 library(readxl)
 library(tidyverse)
 library(emmeans)
-library(ggplot2)
+library(Rmisc)
 
 
 #Coefficient of variation of torque analysis - 10% MVIC
 
 #dataset 10% MVIC----
-torque_steadiness_10 <- read_excel("C:/Users/gabep/Desktop/Study 3/HDEMG paper/paper/final/github/torque steadiness 10.xlsx")
+torque_steadiness_10 <- read_excel("C:/Users/gabep/Desktop/Study 2/HDEMG paper/paper/final/github/torque steadiness 10.xlsx")
 View(torque_steadiness_10)
 
+
 #Shapiro wilk - normality test- 
-torque_steadiness_10 %>% shapiro_test(Covis)
+shapiro.test(torque_steadiness_10$Covis)
 
 #Student T-test
 t10 <- t.test(Covis ~ Groups, torque_steadiness_10, var.equal=TRUE)
@@ -26,11 +28,11 @@ summarySE(torque_steadiness_10, measurevar="Covis", groupvars="Groups")
 #Coefficient of variation of torque analysis - 20% MVIC
 
 #dataset 20% MVIC----
-torque_steadiness_20 <- read_excel("C:/Users/gabep/Desktop/Study 3/HDEMG paper/paper/final/github/torque steadiness 20.xlsx")
+torque_steadiness_20 <- read_excel("C:/Users/gabep/Desktop/Study 2/HDEMG paper/paper/final/github/torque steadiness 20.xlsx")
 View(torque_steadiness_20)
 
 #Shapiro wilk - normality test- 
-torque_steadiness_20 %>% shapiro_test(Covis)
+shapiro.test(torque_steadiness_20$Covis)
 
 #Student T-test
 t20 <- t.test(Covis ~ Groups, torque_steadiness_20, var.equal=TRUE)
